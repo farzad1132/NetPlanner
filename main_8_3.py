b0911ff541a182f0a3d96895d3f113eb42fe804a
MHAZAD <mhazad1999@gmail.com>
2020-02-28 17:30:18 +0300

    the margins in demandmap has been fixed

--- main_8_3.py
+++ main_8_3.py
@@ -58,7 +58,7 @@ from TP1H_Demand.TP1H_R_Demand import TP1H_R_Demand
 
 #from mapwidget import MapWidget
 from mapwidget import MapWidget
-
+import matplotlib as mpl
 from  matplotlib.backends.backend_qt5agg  import  FigureCanvas
 from  matplotlib.figure  import  Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -2219,6 +2219,8 @@ class Ui_MainWindow(object):
 
     def DemandMap_Change(self, Working = None, Protection = None, 
         WorkingRegeneratorsList = None, ProtectionRegenaratorsList = None, WorkingSNR = None, ProtectionSNR = None):
+        #mpl.rcParams["figure.figsize"] = [18.4, 7.8]
+        self.MapWidget.canvas.figure.subplots_adjust(left = -0.04, right = 1, top = 0.85)
         self.MapWidget.canvas.axes.cla()
         R = 6371 
         Source = self.Demand_Source_combobox.currentText()
@@ -2235,7 +2237,6 @@ class Ui_MainWindow(object):
                 self.MapWidget.canvas.axes.plot(x, y, marker ="o", ms=13, color = 'gold')
             else:
                 self.MapWidget.canvas.axes.plot(x, y, marker ="o", ms=13, color = 'black')
-            
 
             self.MapWidget.canvas.axes.text(x, y, NodeName, {'color': 'blueviolet'}, withdash=True,
                 dashdirection= 1,
@@ -2310,14 +2311,12 @@ class Ui_MainWindow(object):
 
                 x, y = self.IdLocationMap[key]
 
-
                 self.MapWidget.canvas.axes.plot(x, y, marker ="o", ms=13, color = 'green')
 
         if ProtectionRegenaratorsList != None:
             for key in ProtectionRegenaratorsList:
 
                 x, y = self.IdLocationMap[key]
-
                 self.MapWidget.canvas.axes.plot(x, y, marker ="o", ms=13, color = 'green')
 
         self.MapWidget.canvas.draw()
